18a95b35d66db6060b6935fb0da714f091e30d90
Alexandre <alexandre@clusterlab.com.br>
2022-04-04 15:54:31 -0300

    added dockerfile for zeromq server

--- apps/sandbox/python/main.py
+++ apps/sandbox/python/main.py
@@ -1,17 +1,23 @@
+#
+#   Hello World server in Python
+#   Binds REP socket to tcp://*:5555
+#   Expects b"Hello" from client, replies with b"World"
+#
+
+import time
 import zmq
 
 context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
 
-#  Socket to talk to server
-print("Connecting to hello world server…")
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
-
-#  Do 10 requests, waiting each time for a response
-for request in range(10):
-    print(f"Sending request {request} …")
-    socket.send(b"Hello")
-
-    #  Get the reply.
+while True:
+    #  Wait for next request from client
     message = socket.recv()
-    print(f"Received reply {request} [ {message} ]")
+    print(f"Received request: {message}")
+
+    #  Do some 'work'
+    time.sleep(1)
+
+    #  Send reply back to client
+    socket.send(b"World")
